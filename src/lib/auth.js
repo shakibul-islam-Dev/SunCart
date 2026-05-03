01a114e2ebb66d13a0cd455e9aac827af6267f24
shakibul-islam-dev <your-noreply-id@users.noreply.github.com>
2026-05-03 15:30:47 +0600

    bug fix-4

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -8,6 +8,9 @@ const db = client.db("suncart");
 // lib/auth.js
 export const auth = betterAuth({
   database: mongodbAdapter(db),
+  emailAndPassword: {
+    enabled: true,
+  },
   // Force a fallback string during build time to prevent the .startsWith() crash
   baseURL: process.env.BETTER_AUTH_URL || "https://sun-cart-hdlk.vercel.app",
   secret: process.env.BETTER_AUTH_SECRET,
